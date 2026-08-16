3ac0a9bbc78412c7935b3243de2e407810bd6247
ShuvamLabs <shuvamchatterjee007@gmail.com>
2026-08-16 23:27:13 +0530

    modified the merged dataset

--- Detector/src/merge-data.py
+++ Detector/src/merge-data.py
@@ -1,23 +1,27 @@
 from pathlib import Path
 import pandas as pd
-import glob
 
 data_dir = Path(r'C:\Users\HP\Documents\PycharmProjects\network-anomaly-detector\Detector\data')
 f_des = data_dir/'UNSW-NB15.csv'
 
-data_files = sorted(
-    f for f in data_dir.glob('*.csv')
-    if f.name != 'UNSW-NB15.csv'
-)
+features = pd.read_csv(data_dir/'UNSW-NB15_features.csv', encoding='ISO-8859-1')
+columns = features["Name"].tolist()
+
+data_files = [
+    data_dir/'UNSW-NB15_1.csv',
+    data_dir/'UNSW-NB15_2.csv',
+    data_dir/'UNSW-NB15_3.csv',
+    data_dir/'UNSW-NB15_4.csv',
+]
 
 first = True
 
 for file in data_files:
-    for chunk in pd.read_csv(file, header = None , chunksize = 100000 , low_memory=False ):
+    for chunk in pd.read_csv(file, header = None,names = columns, chunksize = 100000 , low_memory=False ):
         chunk.to_csv(
             f_des,
+            header = first,
             mode = "w" if first else "a",
-            header = False,
             index = False,
         )
         first = False
